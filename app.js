63f12985718fd1fb522170b0939e301b9d356004
Prathameshp98 <prathameshpatil2812@gmail.com>
2022-02-16 16:48:16 +0530

    level 2 - username and password only

---
+++ app.js
@@ -0,0 +1,83 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/UserDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const secret = "secretKeyForPassword";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+const User = mongoose.model("User",userSchema);
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/login", function(req,res){
+    res.render("login");
+});
+
+app.post("/login", function(req,res){
+
+    User.findOne({email: req.body.username},function(err,foundItem){
+        if(!err){
+            if(foundItem){
+
+                if(foundItem.password === req.body.password){
+                    res.render("secrets");
+                } else {
+                    res.send("Incorrect username or passsword");
+                }
+
+            } else {
+                res.send("NO such user");
+            }
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.get("/register", function(req,res){
+    res.render("register");
+});
+
+app.post("/register", function(req,res){
+
+    const newUser = new User ({
+        email: req.body.username,
+        password: req.body.password
+    }); 
+
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets");
+        } else {
+            res.send(err);
+        }
+    });
+
+});
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("server is up and running.");
+});
